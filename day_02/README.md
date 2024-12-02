2a304d0f322897abd2fc26e5f77bb480c74bb829
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2024-12-02 00:13:02 -0500

    day 2

--- day_02/README.md
+++ day_02/README.md
@@ -1,1 +1,3 @@
 # Day 2
+
+Nice and chill today, easy to brute force part 2 due to the small inputs.
--- day_02/solution.py
+++ day_02/solution.py
@@ -1,67 +1,9 @@
 #!/bin/python3
 
 import sys
-from collections import Counter, defaultdict
-from copy import deepcopy
-from heapq import heappop, heappush
-from typing import List, Set, Tuple
+from typing import List
 
-import numpy as np
-
-sys.setrecursionlimit(100000)
 FILE = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
-
-
-def demo_z3_solver():
-    # Who needs to think when you can z3
-    import z3
-
-    lines = read_lines_to_list()
-    answer = 0
-
-    solver = z3.Solver()
-    x, y, z, vx, vy, vz = [
-        z3.BitVec(var, 64) for var in ["x", "y", "z", "vx", "vy", "vz"]
-    ]
-
-    # 4 unknowns, so we just need 4 equations... I think.
-    for itx in range(4):
-        (cpx, cpy, cpz), (cvx, cvy, cvz) = lines[itx]
-
-        t = z3.BitVec(f"t{itx}", 64)
-        solver.add(t >= 0)
-        solver.add(x + vx * t == cpx + cvx * t)
-        solver.add(y + vy * t == cpy + cvy * t)
-        solver.add(z + vz * t == cpz + cvz * t)
-
-    if solver.check() == z3.sat:
-        model = solver.model()
-        (x, y, z) = (model.eval(x), model.eval(y), model.eval(z))
-        answer = x.as_long() + y.as_long() + z.as_long()
-
-
-def demo_network():
-    from networkx import Graph, connected_components, minimum_edge_cut
-
-    lines = read_lines_to_list()
-    answer = 1
-
-    graph = Graph()
-
-    for node, connections in lines:
-        graph.add_node(node)
-        for connection in connections:
-            graph.add_node(connection)
-            graph.add_edge(
-                *((node, connection) if node > connection else (connection, node))
-            )
-
-    cut = minimum_edge_cut(graph)
-    graph.remove_edges_from(cut)
-
-    components = connected_components(graph)
-    for component in components:
-        answer *= len(component)
 
 
 def read_lines_to_list() -> List[str]:
@@ -78,12 +20,89 @@ def part_one():
     lines = read_lines_to_list()
     answer = 0
 
+    for l in lines:
+        row = [int(a) for a in l.split()]
+
+        is_increasing = None
+        for i in range(0, len(row) - 1):
+            a = row[i]
+            b = row[i + 1]
+
+            if a > b:
+                if is_increasing is None:
+                    is_increasing = False
+                elif is_increasing:
+                    break
+            else:
+                if is_increasing is None:
+                    is_increasing = True
+                elif not is_increasing:
+                    break
+
+            diff = abs(a - b)
+            if diff < 1 or diff > 3:
+                break
+        else:
+            answer += 1
+
     print(f"Part 1: {answer}")
 
 
 def part_two():
     lines = read_lines_to_list()
     answer = 0
+
+    for l in lines:
+        row = [int(a) for a in l.split()]
+
+        is_increasing = None
+        for i in range(0, len(row) - 1):
+            a = row[i]
+            b = row[i + 1]
+
+            if a > b:
+                if is_increasing is None:
+                    is_increasing = False
+                elif is_increasing:
+                    break
+            else:
+                if is_increasing is None:
+                    is_increasing = True
+                elif not is_increasing:
+                    break
+
+            diff = abs(a - b)
+            if diff < 1 or diff > 3:
+                break
+        else:
+            answer += 1
+            continue
+
+        # Brute force, try it with each number removed
+        for j in range(0, len(row)):
+            new_row = row[:j] + row[j + 1 :]
+            is_increasing = None
+            for i in range(0, len(new_row) - 1):
+                a = new_row[i]
+                b = new_row[i + 1]
+
+                if a > b:
+                    if is_increasing is None:
+                        is_increasing = False
+                    elif is_increasing:
+                        break
+                else:
+                    if is_increasing is None:
+                        is_increasing = True
+                    elif not is_increasing:
+                        break
+
+                diff = abs(a - b)
+                if diff < 1 or diff > 3:
+                    break
+            else:
+                answer += 1
+                break
 
     print(f"Part 2: {answer}")
 
